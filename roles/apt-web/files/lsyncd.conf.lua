3f5d1d91d396fdb72d30fd04eef43a5f8711c402
Satoshi Tanaka <tanarky@gmail.com>
2015-07-16 01:53:21 +0900

    fixed

--- roles/apt-web/files/lsyncd.conf.lua
+++ roles/apt-web/files/lsyncd.conf.lua
@@ -6,14 +6,14 @@ settings = {
 
 s3sync = {
   maxProcesses = 6,
-  onStartup = "s3cmd -c /etc/s3cmd.conf -P -r --delete-removed sync ^source ^target/",
-  onCreate  = "[ -f ^source^pathname ] && s3cmd -c /etc/s3cmd.conf -P put ^source^pathname  ^target^pathname || true",
-  onModify  = "[ -f ^source^pathname ] && s3cmd -c /etc/s3cmd.conf -P put ^source^pathname  ^target^pathname || true",
-  onDelete  = "[ -f ^source^pathname ] && s3cmd -c /etc/s3cmd.conf    del                   ^target^pathname || true",
+  onStartup = "aws s3 sync ^source ^target",
+  onCreate  = "[ -f ^source^pathname ] && aws s3 cp ^source^pathname ^target^pathname || true",
+  onModify  = "[ -f ^source^pathname ] && aws s3 cp ^source^pathname ^target^pathname || true",
+  onDelete  = "[ -f ^source^pathname ] && aws s3 rm ^target^pathname || true",
 }
 
 sync{
   s3sync,
   source = "/var/www/wp-static",
-  target = "s3://wp.tnky.pw",
+  target = "s3://www.pets-video.com",
 }
